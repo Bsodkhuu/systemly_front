45e2e03aaa69eba8893348c6f4238fe13edb579e
amgaa <nexus.amgaa@gmail.com>
2023-03-21 16:05:12 +0800

    employee add function added

--- src/pages/Human/index.jsx
+++ src/pages/Human/index.jsx
@@ -4,7 +4,16 @@ import { useState } from "react";
 import { TextInput, Button, Table, Card, Modal,Avatar, Label, FileInput, Checkbox, Select} from "flowbite-react";
 const Human = () => {
     const [showModal, setShowModal] = useState(false);
-
+    const [employee, setEmployee] = useState({
+      id: '',
+      name: '',
+      phone: '',
+      gender: '',
+      profession: '',
+      startDate: '',
+      isWorking: ''
+    });
+    const [employeeList] = useState([]);
     function openModal(){
         setShowModal(true);
     }
@@ -12,8 +21,39 @@ const Human = () => {
         setShowModal(false);
     }
     function createHuman(){
+        console.log("employeeId:");
+        console.log(employee.id);
+        console.log(employee.name);
+        console.log(employee.phone);
+        console.log(employee.gender);
+        console.log(employee.profession);
+        console.log(employee.startDate);
+        console.log(employee.isWorking);
+        employeeList.push({id: employee.id, name: employee.name, profession: employee.profession});
         //fetch
         setShowModal(false);
+    }
+    function onChangeEmployeeId(empId) {
+        employee.id = empId.target.value;
+        console.log(empId.target.value);
+    }
+    function onChangeEmployeeName(event) {
+        employee.name = event.target.value;
+    }
+    function onChangeEmployeePhone(event) {
+        employee.phone = event.target.value;
+    }
+    function onChangeEmployeeGender(event) {
+        employee.gender = event.target.value;
+    }
+    function onChangeEmployeeProfession(event) {
+        employee.profession = event.target.value;
+    }
+    function onChangeEmployeeStartDate(event) {
+        employee.startDate = event.target.value;
+    }
+    function onChangeEmployeeJob(event) {
+        employee.isWorking = event.target.value;
     }
     return (
         <Layout>
@@ -26,13 +66,15 @@ const Human = () => {
                 <div className="mb-2 block">
                   <Label htmlFor="employeeId" value="Ажилтны дугаар" />
                 </div>
-                <TextInput id="employeeId" />
+                  <TextInput id="employeeId"
+                    onChange={onChangeEmployeeId}/>
               </div>
               <div className="w-1/2">
                 <div className="mb-2 block">
                   <Label htmlFor="name" value="Нэр" />
                 </div>
-                <TextInput id="name"/>
+                <TextInput id="name"
+                  onChange={onChangeEmployeeName}/>
               </div>
             </div>
             <div className="flex gap-4">
@@ -40,14 +82,16 @@ const Human = () => {
                 <div className="mb-2 block">
                   <Label htmlFor="phone" value="Утасны дугаар" />
                 </div>
-                <TextInput id="phone" />
+                <TextInput id="phone"
+                  onChange={onChangeEmployeePhone}/>
               </div>
               <div className="w-1/2 flex gap-4">
                 <div className="w-1/2">
                   <div className="mb-2 block">
                     <Label htmlFor="gender" value="Хүйс" />
                   </div>
-                  <TextInput type="text" id="gender"/>
+                  <TextInput type="text" id="gender"
+                    onChange={onChangeEmployeeGender}/>
                 </div>
               </div>
             </div>
@@ -67,30 +111,32 @@ const Human = () => {
                     <div className="mb-2 block">
                         <Label htmlFor="profession" value="Мэргэжил"/>
                     </div>
-                    <TextInput id="profession" />
+                    <TextInput id="profession"
+                      onChange={onChangeEmployeeProfession}/>
                 </div>
                 <div className="w-1/2"> 
                 <div className="mb-2 block">
                   <Label htmlFor="startDate" value="Ажилд орсон он сар" /> 
                 </div>
-                <TextInput id="startDate" type="date" />
+                <TextInput id="startDate" type="date"
+                  onChange={onChangeEmployeeStartDate}/>
                 </div>
             </div>
             <div className="flex gap-4">
                 <div className="w-1/2">
                     <div className="mb-2 block">
-                        <Label htmlFor="job" value="Ажиллаж байгаа эсэх"/>
+                      
+  <Label htmlFor="job" value="Ажиллаж байгаа эсэх"/>
                     </div>
-                    <Select> 
-                        <option value="job">Ажиллаж байгаа</option>
-                        <option value="job">Ээлжийн амралт</option>
-                        <option value="job">Томилолт</option>
-                        <option value="job">Ажлаас гарсан</option>
+                    <Select
+                      onChange={onChangeEmployeeJob}> 
+                        <option value="working">Ажиллаж байгаа</option>
+                        <option value="vacation">Ээлжийн амралт</option>
+                        <option value="tour">Томилолт</option>
+                        <option value="quit">Ажлаас гарсан</option>
                         </Select>
                 </div>
-
-                </div>
-            
+            </div>
           </form>
         </Modal.Body>
         <Modal.Footer>
@@ -141,6 +187,28 @@ const Human = () => {
                 <Table.HeadCell>Мэргэжил</Table.HeadCell>
               </Table.Head>
               <Table.Body> 
+                {
+                  employeeList.map(employee => 
+                    <Table.Row key={employee.id}>
+                      <Table.Cell>
+                        <Checkbox/>
+                      </Table.Cell>
+                      <Table.Cell>
+                        <Avatar src="">
+                        </Avatar>
+                      </Table.Cell>
+                      <Table.Cell key={employee.id}>
+                        {employee.id}
+                      </Table.Cell>
+                      <Table.Cell>
+                        {employee.name}
+                      </Table.Cell>
+                      <Table.Cell>
+                        {employee.profession}
+                      </Table.Cell>
+                   </Table.Row>
+                  )
+                }
                 <Table.Row>
                   <Table.Cell>
                     <Checkbox/>
