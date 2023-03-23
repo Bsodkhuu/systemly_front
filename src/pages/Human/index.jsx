14969e7469829e0b2a881f64018e32f72727491d
amgaa <nexus.amgaa@gmail.com>
2023-03-23 16:53:05 +0800

    employee modified

--- src/pages/Human/index.jsx
+++ src/pages/Human/index.jsx
@@ -8,6 +8,7 @@ import {
   TextInput,
   Select,
   ListGroup,
+  Avatar,
   Dropdown, Textarea, FileInput, Checkbox
 } from "flowbite-react";
 
@@ -17,7 +18,35 @@ import {useState } from "react";
 
     const [showModal, setShowModal] = useState(false);
     const [showSearch, setShowSearch] = useState();
-
+    const [employee, setEmployee] = useState({
+      lastName: '',
+      firstName: '',
+      phone: '',
+      email: '',
+      register: '',
+      profession: '',
+      avatar: '',
+      checked: false
+    });
+    const [employeeList] = useState([]);
+    const [showServiceList, setShowServiceList] = useState([]);
+    const [serviceList] = useState([
+      {
+        id: 'ID123',
+        name: 'Tos solih',
+        employeeId: 'RD11223345'
+      },
+      {
+        id: 'ID12345',
+        name: 'Tosnii shuur solih',
+        employeeId: 'RD11223345'
+      },
+      {
+        id: 'ID11223345',
+        name: 'Buten ugaalga',
+        employeeId: 'RD11223347'
+      }
+    ]);
     function openModal(){
       setShowModal(true);
     }
@@ -26,11 +55,61 @@ import {useState } from "react";
     }
 
     function createHuman(){
+      console.log(employee.lastName);
+      console.log(employee.firstName);
+      console.log(employee.phone);
+      console.log(employee.email);
+      console.log(employee.register);
+      console.log(employee.profession);
+      console.log(employee.avatar);
+      employeeList.push({register: employee.register,
+        lastName: employee.lastName,
+        firstName: employee.firstName,
+        profession: employee.profession,
+        checked: false});
       setShowModal(false);
     }
 
     function search(){
       setShowSearch();
+    }
+
+    function onChangeEmployeeLastName(event) {
+      employee.lastName = event.target.value;
+    }
+    function onChangeEmployeeFirstName(event) {
+      employee.firstName = event.target.value;
+    }
+    function onChangeEmployeePhone(event) {
+      employee.phone = event.target.value;
+    }
+    function onChangeEmployeeEmail(event) {
+      employee.email = event.target.value;
+    }
+    function onChangeEmployeeRegister(event) {
+      employee.register = event.target.value;
+    }
+    function onChangeEmployeeProfession(event) {
+      employee.profession = event.target.value;
+    }
+    function onChangeEmployeeAvatar(event) {
+      employee.avatar = event.target.value;
+    }
+    function employeeService(register) {
+      console.log(register);
+      var foundEmployee = employeeList.find((employee) =>
+        employee.register == register);
+      console.log(foundEmployee);
+      var foundService = serviceList.filter((service) =>
+        service.employeeId == foundEmployee.register);
+      console.log(foundService);
+      setShowServiceList(foundService);
+    }
+    function employeeActive() {
+      console.log('active');
+    }
+    function employeeHistory() {
+      console.log('history');
     }
     return(
       <Layout> 
@@ -41,15 +120,17 @@ import {useState } from "react";
               <div className="flex gap-4">
                 <div className="w-1/2">
                   <div className="mb-2 block">
-                    <Label htmlFor="firstName" value="Овог" />
+                    <Label htmlFor="lastName" value="Овог" />
                   </div>
-                  <TextInput id="firstName"/>
+                  <TextInput id="lastName"
+                    onChange={onChangeEmployeeLastName}/>
                 </div>
                 <div className="w-1/2">
                   <div className="mb-2 block">
-                    <Label htmlFor="lastName" value="Нэр"/>
+                    <Label htmlFor="firstName" value="Нэр"/>
                   </div>
-                  <TextInput id="lastName" type="text"/>
+                  <TextInput id="firstName" type="text"
+                    onChange={onChangeEmployeeFirstName}/>
                 </div>
               </div>
               <div className="flex gap-4">
@@ -57,13 +138,15 @@ import {useState } from "react";
                   <div className="mb-2 block">
                     <Label htmlFor="phone" value="Утасны дугаар" />
                   </div>
-                  <TextInput id="phone" />
+                  <TextInput id="phone"
+                    onChange={onChangeEmployeePhone}/>
                 </div>
                 <div className="w-1/2">
                   <div className="mb-2 block">
                     <Label htmlFor="email" value="Имэйл"/>
                   </div>
-                  <TextInput id="email" />
+                  <TextInput id="email"
+                    onChange={onChangeEmployeeEmail}/>
                 </div>
               </div>
               <div className="flex gap-4">
@@ -71,19 +154,22 @@ import {useState } from "react";
                   <div className="mb-2 block">
                     <Label htmlFor="register_number" value="Регистрийн дугаар" />
                   </div>
-                  <TextInput id="register_number"/>
+                  <TextInput id="register_number"
+                    onChange={onChangeEmployeeRegister}/>
                 </div>
                 <div className="w-1/2">
                   <div className="mb-2 block">
                     <Label htmlFor="image" value="Ажилчдын зураг" /> 
                   </div>
-                  <FileInput id="image" />
+                  <FileInput id="image"
+                    onChange={onChangeEmployeeAvatar}/>
                 </div>
                 <div className="w-1/2">
                   <div className="mb-2 block">
                     <Label htmlFor="profession" value="Албан тушаал"/>
                   </div>
-                  <TextInput type="profession"/>
+                  <TextInput type="profession"
+                    onChange={onChangeEmployeeProfession}/>
                 </div>
               </div>
             </form>
@@ -115,13 +201,75 @@ import {useState } from "react";
               <div className="p-4">
                 <Card>
                   <h4 className="text-1xl">Ажилчдын жагсаалт</h4>
-                  zaswarin ajilchdin ners, 
+                  <div className="flex gap-4">
+                    <Button onClick={employeeActive} className="bg-blue-500">Идэвхитэй</Button>
+                    <Button onClick={employeeHistory} className="bg-blue-500">Түүх</Button>
+                    <Button className="bg-blue-500" onClick={employeeService}>Үйлчилгээ</Button>
+                  </div>
+                  <Table>
+                    <Table.Head className="uppercase">
+                      <Table.HeadCell>
+                        <Checkbox/>
+                      </Table.HeadCell>
+                      <Table.HeadCell>Ажилтаны зураг</Table.HeadCell>
+                      <Table.HeadCell>Овог</Table.HeadCell>
+                      <Table.HeadCell>Нэр</Table.HeadCell>
+                      <Table.HeadCell>Мэргэжил</Table.HeadCell>
+                      <Table.HeadCell>Үйлдэл</Table.HeadCell>
+                    </Table.Head>
+                    <Table.Body>
+                      {
+                        employeeList.map(employee =>
+                          <Table.Row key={employee.register}>
+                            <Table.Cell>
+                              <Checkbox checked={employee.checked}/>
+                            </Table.Cell>
+                            <Table.Cell>
+                              <Avatar src={employee.avatar}>
+                              </Avatar>
+                            </Table.Cell>
+                            <Table.Cell>
+                              {employee.lastName}
+                            </Table.Cell>
+                            <Table.Cell>
+                              {employee.firstName}
+                            </Table.Cell>
+                            <Table.Cell>
+                              {employee.profession}
+                            </Table.Cell>
+                            <Table.Cell>
+                              <Button onClick={() => employeeHistory(employee.register)}>Түүх</Button>
+                            </Table.Cell>
+                          </Table.Row>
+                        )
+                      }
+                    </Table.Body>
+                  </Table>
                 </Card>
                 </div>
               <div className="p-4">
                 <Card>
                   <h4 className="text-1xl">Хийсэн үйлчилгээний жагсаалт</h4>
-                  zawsar duusan id 
+                  <Table>
+                    <Table.Head className="uppercase">
+                      <Table.HeadCell>Үйлчилгээний нэр</Table.HeadCell>
+                      <Table.HeadCell>Ажилтаны регистер</Table.HeadCell>
+                    </Table.Head>
+                    <Table.Body>
+                      {
+                        showServiceList.map(service =>
+                          <Table.Row key={service.id}>
+                            <Table.Cell>
+                              {service.name}
+                            </Table.Cell>
+                            <Table.Cell>
+                              {service.employeeId}
+                            </Table.Cell>
+                          </Table.Row>
+                        )
+                      }
+                    </Table.Body>
+                  </Table>
                 </Card>
               </div>
               <div className="p-4">
