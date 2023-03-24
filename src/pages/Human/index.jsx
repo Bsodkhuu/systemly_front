8d31e34be6a358ee80cb848a3bfcdad8d86c3338
amgaa <nexus.amgaa@gmail.com>
2023-03-24 15:48:53 +0800

    employee functions added

--- src/pages/Human/index.jsx
+++ src/pages/Human/index.jsx
@@ -12,7 +12,7 @@ import {
   Dropdown, Textarea, FileInput, Checkbox
 } from "flowbite-react";
 
-import {useState } from "react";
+import {useState, useEffect} from "react";
 
   const Human = ()=> {
 
@@ -25,26 +25,41 @@ import {useState } from "react";
       email: '',
       register: '',
       profession: '',
-      avatar: '',
-      checked: false
+      avatar: ''
     });
     const [employeeList] = useState([]);
     const [showServiceList, setShowServiceList] = useState([]);
+    const [showServiceDetailList, setShowServiceDetailList] = useState([]);
     const [serviceList] = useState([
       {
         id: 'ID123',
         name: 'Tos solih',
-        employeeId: 'RD11223345'
+        employeeId: 'RD11223345',
+        qty: '2',
+        price: '10.000',
+        discount: '5%',
+        total: '20.000',
+        plateNumber: '1235УБА'
       },
       {
         id: 'ID12345',
         name: 'Tosnii shuur solih',
-        employeeId: 'RD11223345'
+        employeeId: 'RD11223345',
+        qty: '3',
+        price: '10.000',
+        discount: '5%',
+        total: '35.000',
+        plateNumber: '2345УБА'
       },
       {
         id: 'ID11223345',
         name: 'Buten ugaalga',
-        employeeId: 'RD11223347'
+        employeeId: 'RD11223347',
+        qty: '5',
+        price: '15.000',
+        discount: '15%',
+        total: '55.000',
+        plateNumber: '3445УБЭ'
       }
     ]);
     function openModal(){
@@ -65,8 +80,7 @@ import {useState } from "react";
       employeeList.push({register: employee.register,
         lastName: employee.lastName,
         firstName: employee.firstName,
-        profession: employee.profession,
-        checked: false});
+        profession: employee.profession});
       setShowModal(false);
     }
 
@@ -95,28 +109,72 @@ import {useState } from "react";
     function onChangeEmployeeAvatar(event) {
       employee.avatar = event.target.value;
     }
-    function employeeChecked(event, register) {
+    function checkAllEmployees(event) {
+      const employeeRows = Array.from(document.getElementsByClassName('employee_row'));
+      employeeRows.forEach((row) => {
+        var checkbox = row.childNodes[0].childNodes[0];
+        if (event.target.checked == false) {
+          checkbox.checked = false;
+        } else {
+          checkbox.checked = true;
+        }
+      });
+    }
+    function checkAllServices(event) {
+      const serviceRows = Array.from(document.getElementsByClassName('service_row'));
+      serviceRows.forEach((row) => {
+        var checkbox = row.childNodes[0].childNodes[0];
+        if (event.target.checked == false) {
+          checkbox.checked = false;
+        } else {
+          checkbox.checked = true;
+        }
+      });
+    }
+/*    function employeeChecked(event, register) {
       console.log(event.target.checked);
       console.log(register);
       var foundEmployee = employeeList.find((employee) => employee.register == register);
       foundEmployee.checked = event.target.checked;
       console.log(employeeList);
     }
+*/
     function employeeService() {
-      var checkedEmployees = employeeList.filter((employee) =>
-        employee.checked == true);
-      console.log(checkedEmployees);
-      var foundService = serviceList.filter((service) =>
-        checkedEmployees.find((employee) =>
-          service.employeeId == employee.register));
-      console.log(foundService);
-      setShowServiceList(foundService);
+      const employeeRows = Array.from(
+        document.getElementsByClassName('employee_row')
+      );
+      var tempList = [];
+      employeeRows.forEach(employee => {
+        var checked = employee.childNodes[0].childNodes[0].checked;
+        if (checked == true) {
+          var foundServices = serviceList.filter(service => service.employeeId == employee.id);
+          tempList = tempList.concat(foundServices);
+        }
+      });
+      setShowServiceList(tempList);
     }
     function employeeActive() {
       console.log('active');
     }
     function employeeHistory() {
       console.log('history');
+    }
+    function serviceDetails() {
+      const serviceRows = Array.from(document.getElementsByClassName('service_row'));
+      var tempList = [];
+      serviceRows.forEach(serviceNode => {
+        var checked = serviceNode.childNodes[0].childNodes[0].checked;
+        if (checked == true) {
+          var foundServices = serviceList.filter(service => service.id == serviceNode.id);
+          tempList = tempList.concat(foundServices);
+        }
+      });
+      setShowServiceDetailList(tempList);
+    }
+    function checkService(event, id) {
+      console.log(event.target.checked);
+      var foundService = serviceList.find((service) => service.id == id);
+      foundService.checked = event.target.checked;
     }
     return(
       <Layout> 
@@ -194,14 +252,16 @@ import {useState } from "react";
           <div className="bg-white p-6 rounded-lg">
             <div className="flex justify-between mb-4">
               <h4 className="text-1xl">Ажилчид</h4>
+              <Button className="bg-blue-500" onClick={openModal}>
+                Ажилчид нэмэх
+              </Button>
               <div className="flex gap-4">
-                <TextInput id="search" type="search" placeholder="Хайх"/>
+                <TextInput id="startDate" type="date"/>
+                -
+                <TextInput id="endDate" type="date"/>
                 <Button className="bg-blue-500" onClick={search}>
                   Хайх
                 </Button>
-                <Button className="bg-blue-500" onClick={openModal}>
-                 Ажилчид нэмэх
-              </Button>
               </div>
             </div>
             <div className="grid grid-cols-3">
@@ -216,7 +276,7 @@ import {useState } from "react";
                   <Table>
                     <Table.Head className="uppercase">
                       <Table.HeadCell>
-                        <Checkbox/>
+                        <Checkbox onChange={checkAllEmployees}/>
                       </Table.HeadCell>
                       <Table.HeadCell>Ажилтаны зураг</Table.HeadCell>
                       <Table.HeadCell>Овог</Table.HeadCell>
@@ -226,9 +286,10 @@ import {useState } from "react";
                     <Table.Body>
                       {
                         employeeList.map(employee =>
-                          <Table.Row key={employee.register}>
+                          <Table.Row key={employee.register} id={employee.register} className="employee_row">
                             <Table.Cell>
-                              <Checkbox onChange={() => employeeChecked(event, employee.register)}/>
+{/*                             <Checkbox className="employee_checkboxes" onChange={() => employeeChecked(event, employee.register)}/> */}
+                              <Checkbox/>
                             </Table.Cell>
                             <Table.Cell>
                               <Avatar src={employee.avatar}>
@@ -253,20 +314,29 @@ import {useState } from "react";
               <div className="p-4">
                 <Card>
                   <h4 className="text-1xl">Хийсэн үйлчилгээний жагсаалт</h4>
+                  <div className="flex gap-4">
+                    <Button className="bg-blue-500" onClick={serviceDetails}>Дэлгэрэнгүй</Button>
+                  </div>
                   <Table>
                     <Table.Head className="uppercase">
-                      <Table.HeadCell>Үйлчилгээний нэр</Table.HeadCell>
-                      <Table.HeadCell>Ажилтаны регистер</Table.HeadCell>
+                      <Table.HeadCell>
+                        <Checkbox onChange={checkAllServices}/>
+                      </Table.HeadCell>
+                      <Table.HeadCell>Захиалгын дугаар</Table.HeadCell>
+                      <Table.HeadCell>Улсын дугаар</Table.HeadCell>
                     </Table.Head>
                     <Table.Body>
                       {
                         showServiceList.map(service =>
-                          <Table.Row key={service.id}>
+                          <Table.Row key={service.id} id={service.id} className="service_row">
                             <Table.Cell>
-                              {service.name}
+                              <Checkbox/>
                             </Table.Cell>
                             <Table.Cell>
-                              {service.employeeId}
+                              {service.id}
+                            </Table.Cell>
+                            <Table.Cell>
+                              {service.plateNumber}
                             </Table.Cell>
                           </Table.Row>
                         )
@@ -275,7 +345,9 @@ import {useState } from "react";
                   </Table>
                 </Card>
               </div>
-              <div className="p-4">
+            </div>
+          </div>
+            <div className="p-4">
                 <Card>
                   <h4 className="text-1xl">Үйлчилгээний дэлгэрэнгүй</h4>
                   <Table> 
@@ -289,23 +361,36 @@ import {useState } from "react";
                       <Table.HeadCell>Нийт</Table.HeadCell>
                     </Table.Head>
                     <Table.Body className="divide-y">
-                      <Table.Row>
-                        <Table.Cell>Example</Table.Cell>
-                        <Table.Cell>2</Table.Cell>
-                        <Table.Cell>10.000</Table.Cell>
-                        <Table.Cell>Example</Table.Cell>
-                        <Table.Cell>Calculate</Table.Cell>
-                        <Table.Cell>20.000</Table.Cell>
-                      </Table.Row>
+                      {
+                        showServiceDetailList.map(service =>
+                          <Table.Row key={service.id}>
+                            <Table.Cell>
+                              {service.name}
+                            </Table.Cell>
+                            <Table.Cell>
+                              {service.qty}
+                            </Table.Cell>
+                            <Table.Cell>
+                              {service.price}
+                            </Table.Cell>
+                            <Table.Cell>
+                              {service.employeeId}
+                            </Table.Cell>
+                            <Table.Cell>
+                              {service.discount}
+                            </Table.Cell>
+                            <Table.Cell>
+                              {service.total}
+                            </Table.Cell>
+                          </Table.Row>
+                        )
+                      }
                     </Table.Body>
                   </Table>
                 </Card>
               </div>
-            </div>
-          </div>
         </div>
       </Layout>
     );
   }
-
-  export default Human;
+export default Human;
