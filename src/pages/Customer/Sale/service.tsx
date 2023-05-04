727427680233da288b5a5270377b8c9dab747d20
xkalf0710 <106955580+xkalf0710@users.noreply.github.com>
2023-05-04 15:07:21 +0800

    url

--- src/pages/Customer/Sale/service.tsx
+++ src/pages/Customer/Sale/service.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Layout from "../../../components/layout";
-import { Label, TextInput } from "flowbite-react";
+import { Button, Label, TextInput } from "flowbite-react";
 import { useForm } from "react-hook-form";
 import { useMutation } from "react-query";
 import { axiosClient } from "../../../config/axios";
@@ -10,10 +10,10 @@ export interface ServiceHistory {
     id: string;
     createdAt: string;
     updatedAt: string;
-    vinNumberId?: string;
+    vinNumberId?: string;  //garagacustomervehicles 
     serviceDate: string;
-    serviceAffiliateId?: string;
-    ajilGuitsetgesenAjiltanId?: string;
+    serviceAffiliateId?: string; //user
+    ajilGuitsetgesenAjiltanId?: string; // affiliateemployee
     quantity: number;
     discount: number;
     netPrice: number;
@@ -26,7 +26,12 @@ const Vilchilgee = () => {
     const { mutateAsync } = useMutation("serviceHistory", serviceHistory);
 
     async function serviceHistory(values: ServiceHistory) {
-        const response = await axiosClient.post("/service_histories", values);
+        const response = await axiosClient.post("/service_histories",{
+            ...values, 
+            quantity: parseInt(values.quantity.toString()), 
+            discount: parseFloat(values.discount.toString()), 
+            netPrice: parseInt(values.netPrice.toString()),
+        });
         return response.data;
     }
 
@@ -41,19 +46,19 @@ const Vilchilgee = () => {
                     <div className="flex justify-between mb-4">
                         <h4 className="text-1xl">Үйлчилгээний дэлгэрэнгүй</h4>
                     </div>
-                    <form className="flex flex-col gap-4">
+                    <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-4">
                         <div className="flex gap-4">
                             <div className="w-1/2">
                                 <div className="mb-2 block">
                                     <Label htmlFor="serviceName" value="Үйлчилгээний нэр"/>
                                 </div>
-                                <TextInput id="serviceName"  />
+                                <TextInput id="serviceName" {...register("serviceName")}/>
                             </div>
                             <div className="w-1/2">
                                 <div className="mb-2 block">
-                                    <Label htmlFor="ajilGuitsetgesenAjiltan" value="Үйлчилгээ хийсэн механикч"/>
+                                    <Label htmlFor="ajilGuitsetgesenAjiltanId" value="Үйлчилгээ хийсэн механикч"/>
                                 </div>
-                                <TextInput id="ajilGuitsetgesenAjiltan"  />
+                                <TextInput id="ajilGuitsetgesenAjiltanId" {...register("ajilGuitsetgesenAjiltanId")}/>
                             </div>
                         </div>
                         <div className="flex gap-4">
@@ -61,13 +66,13 @@ const Vilchilgee = () => {
                                 <div className="mb-2 block">
                                     <Label htmlFor="quantity" value="Тоо ширхэг"/>
                                 </div>
-                                <TextInput id="quantity"  />
+                                <TextInput id="quantity"  {...register("quantity")}/>
                             </div>
                             <div className="w-1/2">
                                 <div className="mb-2 block">
                                     <Label htmlFor="netPrice" value="Нэгжийн үнэ"/>
                                 </div>
-                                <TextInput id="netPrice"  />
+                                <TextInput id="netPrice" {...register("netPrice")} />
                             </div>
                         </div>
                         <div className="flex gap-4">
@@ -75,15 +80,28 @@ const Vilchilgee = () => {
                                 <div className="mb-2 block">
                                     <Label htmlFor="discount" value="Хямдрал"/>
                                 </div>
-                                <TextInput id="discount"  />
+                                <TextInput id="discount"  {...register("discount")}/>
                             </div>
                             
                             <div className="w-1/2">
                                 <div className="mb-2 block">
                                     <Label htmlFor="serviceDate" value="Дараагийн үйлчилгээний хуваарь"/>
                                 </div>
-                                <TextInput type="date" id="serviceDate" />
+                                <TextInput type="date" id="serviceDate" {...register("serviceDate")}/>
                             </div>
+                        </div>
+                        <div className="flex gap-4">
+                            <div className="w-1/2">
+                                <div className="mb-2 block">
+                                    <Label htmlFor="vinNumberId" value="vinNumberId"/>
+                                </div>
+                                <TextInput id="vinNumberId"  {...register("vinNumberId")}/>
+                            </div>
+
+                        </div>
+                        <div className="flex gap-4">
+                        <Button className="bg-gray-400">Буцах</Button>
+                        <Button onClick={handleSubmit(onSubmit)} className="bg-orange-400">Хадгалах</Button>
                         </div>
                     </form>
                 </div>
