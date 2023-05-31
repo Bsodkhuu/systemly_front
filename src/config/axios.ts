731e32cf6e49dc2a59ca6dcc7cd4885a51ce9a1a
xkalf0710 <106955580+xkalf0710@users.noreply.github.com>
2023-05-31 08:47:40 +0800

    11

--- src/config/axios.ts
+++ src/config/axios.ts
@@ -2,6 +2,5 @@ import axios from "axios";
 
 export const axiosClient = axios.create({
   baseURL: "https://systemlyerp-production.up.railway.app",
-  // baseURL: "http://localhost:3000",
   withCredentials: true,
 });
